ba3eea2ae82208cc14d89e6d4ca68299e0bcebc8
Chisom Laura <102640478+ChisomLaura@users.noreply.github.com>
2022-06-20 17:41:22 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var footer = document.querySelector("footer");
+console.log(footer);
+footer.style.backgroundColor = "#d7ddf3";
+
+var tagline = document.querySelector(".tagline");
+console.log(tagline);
+tagline.innerText = "Express yourself freely.";
+
+var address = document.querySelector("address");
+console.log(address);
+address.style.lineHeight = "1.5em";
+address.style.fontSize = "1.75em";
+
+var everyDay = document.querySelector("h3");
+everyDay.innerHTML = "We're here for you <em> everyday </em> of the week.";
